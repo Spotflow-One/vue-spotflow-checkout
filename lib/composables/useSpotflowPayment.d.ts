79fb5ebba1742eb83aca256bf8b1fb5ca05ad10a
Oluwatomisin Jimoh <jayoluwatosin@gmail.com>
2025-10-15 13:59:44 +0100

    chore: remove type declaration file

--- lib/composables/useSpotflowPayment.d.ts
+++
@@ -1,7 +0,0 @@
-import type { SpotflowPaymentOptions } from '../types';
-declare global {
-    interface Window {
-        SpotflowCheckout: any;
-    }
-}
-export declare function useSpotflowPayment(): (options: SpotflowPaymentOptions) => Promise<void>;
